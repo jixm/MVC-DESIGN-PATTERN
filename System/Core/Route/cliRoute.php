f99c3b2e9a10b839392c12dfe5402be4a82f173c
jelty <xm_hithd@163.com>
2015-11-06 10:20:34 +0100

    add cli route

--- System/Core/Route/cliRoute.php
+++ System/Core/Route/cliRoute.php
@@ -6,6 +6,19 @@ use System\Core\Route;
 class cliRoute extends Route {
 	
 	public function __construct() {
-		parent::__construct('adsf/adsf/asdfasdf/adsf/');
+		global $argv;
+		if(isset($argv[1]) && $argv[1]){
+			if(preg_match('/request_uri/',$argv[1])){
+				$uri = substr($argv[1],12);
+			}else{
+				echo 'be sure params is right';exit;
+			}
+		}else{
+			echo 'lack of params';exit;
+		}
+		if(isset($argv[2])){
+			$_GET = $argv[2];
+		}
+		parent::__construct($uri);
 	}
 }
